a1c6aaeff8e75ffd81a0034f8760f784c920a2ad
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-05-27 12:30:47 +0600

    Add processBlock

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 const R = require('ramda')
 
-const getClassList = exports.getClassList = R.pipe(
+const getClassList = R.pipe(
   R.unless(R.is(String), () => { throw new Error('getClassList argument should be a string') }),
   R.split(' '),
   R.reject(R.equals(''))
 )
+
+exports.getClassList = getClassList
+
+
+const startWith = str => R.pipe(
+  R.take(str.length),
+  R.equals(str)
+)
+
+const processBlock = (config, node) => {
+  const classList = getClassList(node.attrs.class)
+  const isBlock = startWith(config.blockPrefix)
+
+  const getBlockName = R.pipe(
+    R.find(isBlock),
+    R.slice(config.blockPrefix.length, Infinity)
+  )
+
+  const blockName = getBlockName(classList)
+  const newClassList = R.reject(isBlock, classList)
+
+  return R.pipe(
+    R.assocPath(['attrs', 'block'], blockName),
+    newClassList.length
+      ? R.assocPath(['attrs', 'class'], R.join(' ', newClassList))
+      : R.dissocPath(['attrs', 'class'])
+  )(node)
+}
+
+exports.processBlock = processBlock
+
 
 exports.default = config => { // eslint-disable-line
   return function posthtmlBemSugar(tree) {
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const posthtml = require('posthtml')
-const { default: bemSugar, getClassList } = require('./src/')
+const { default: bemSugar, getClassList, processBlock } = require('./src/')
 require('chai').should()
 
 
@@ -18,30 +18,70 @@ const testCreator = defaultConfig => (discription, input, output, config) => {
   })
 }
 
-const test = testCreator({
+const defaults = {
   blockPrefix: '-',
   elemPrefix: '__',
   modPrefix: '_',
   modDlmtr: '_',
-})
+}
 
-describe('utilities', () => {
+const test = testCreator(defaults)
+
+describe('getClassList', () => {
   it('getClassList validation', () => {
     (() => getClassList(123)).should.throw('getClassList argument should be a string')
   })
 
-  it('getClassList', () => {
+  it('getClassList should right process emtry string', () => {
+    getClassList('').should.be.eql([])
+  })
+
+  it('getClassList should right process one class', () => {
+    getClassList('class').should.be.eql(['class'])
+  })
+
+  it('getClassList should right process few classes', () => {
     getClassList('some classes').should.be.eql(['some', 'classes'])
+  })
+
+  it('getClassList should right process extra spaces', () => {
     getClassList('some  classes').should.be.eql(['some', 'classes'])
     getClassList('   some  classes   ').should.be.eql(['some', 'classes'])
+  })
+
+  it('getClassList should right process falsy values', () => {
     getClassList('0 false null undefined').should.be.eql(['0', 'false', 'null', 'undefined'])
   })
 })
 
-describe('block', () => {
-  test(
-    'some',
-    '<div></div>',
-    '<div></div>'
-  )
+
+describe('processBlock', () => {
+  it('processBlock should remove empty class', () => {
+    const tree = {
+      tag: 'div',
+      attrs: { class: '-block' },
+    }
+
+    processBlock(defaults, tree).should.be.eql({
+      tag: 'div',
+      attrs: {
+        block: 'block',
+      },
+    })
+  })
+
+  it('processBlock should keep another classes', () => {
+    const tree = {
+      tag: 'div',
+      attrs: { class: '-block js-something' },
+    }
+
+    processBlock(defaults, tree).should.be.eql({
+      tag: 'div',
+      attrs: {
+        block: 'block',
+        class: 'js-something',
+      },
+    })
+  })
 })
